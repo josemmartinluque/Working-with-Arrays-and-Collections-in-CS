05114ae10a157628abeb484d81ab87d3b59303ce
josemmartinluque <jose.m.martin.luque@avanade.com>
2024-04-04 12:13:49 +0200

    Module 3 completed

---
+++ FindBusTo/BusRouteRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluralsight.ArraysCollections.Demos
+{
+	public class BusRouteRepository
+	{
+		public static BusRoute[] InitializeRoutes()
+		{
+			return new BusRoute[] {
+				new BusRoute(40, new string[] {
+					"Morecambe", "Lancaster", "Garstang", "Preston" }),
+				new BusRoute(42, new string[] { "Lancaster", "Garstang", "Blackpool" }),
+				new BusRoute(100, new string[] { "University", "Lancaster", "Morecambe" }),
+				new BusRoute(555, new string[] {
+					"Lancaster", "Carnforth", "Kendal", "Windermere", "Keswick" })
+			};
+		}
+	}
+}
---
+++ FindBusTo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pluralsight.ArraysCollections.Demos
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BusRoute[] allRoutes = BusRouteRepository.InitializeRoutes();
+
+			Console.WriteLine("Where do you want to go to?");
+			string location = Console.ReadLine();
+
+			BusRoute[] routes = FindBusesTo(allRoutes, location);
+
+			if (routes.Length > 0)
+				foreach (BusRoute route in routes)
+					Console.WriteLine($"You can use route {route}");
+			else
+				Console.WriteLine($"No routes go to {location}");
+		}
+
+		public static BusRoute[] FindBusesTo(BusRoute[] routes, string location)
+		{
+			return Array.FindAll(routes, route => route.Serves(location));
+		}
+	}
+}
