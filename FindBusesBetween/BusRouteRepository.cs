254649027423ca45724cb72b4771187574ae5ad5
josemmartinluque <jose.m.martin.luque@avanade.com>
2024-04-04 12:56:44 +0200

    Module 6 completed

---
+++ FindBusesBetween/BusRouteRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluralsight.ArraysCollections.Demos
+{
+	public class BusRouteRepository
+	{
+		private readonly BusRoute[] _allRoutes;
+		public BusRouteRepository()
+		{
+			_allRoutes = new BusRoute[] {
+				new BusRoute(40, new string[] {
+					"Morecambe", "Lancaster", "Garstang", "Preston" }),
+				new BusRoute(42, new string[] { "Lancaster", "Garstang", "Blackpool" }),
+				new BusRoute(100, new string[] { "University", "Lancaster", "Morecambe" }),
+				new BusRoute(555, new string[] {
+					"Lancaster", "Carnforth", "Kendal", "Windermere", "Keswick" }),
+				new BusRoute(5, new string[] { "Overton", "Morecambe", "Carnforth"})
+			};
+		}
+		public BusRoute[] FindBusesTo(string location)
+		{
+			return Array.FindAll(_allRoutes, route => route.Serves(location));
+		}
+		public BusRoute[] FindBusesBetween(string location1, string location2)
+		{
+			return Array.FindAll(_allRoutes,
+				route => route.Serves(location1) && route.Serves(location2));
+		}
+	}
+}
---
+++ FindBusesBetween/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pluralsight.ArraysCollections.Demos
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BusRouteRepository repository = new BusRouteRepository();
+
+			Console.WriteLine("Where are you?");
+			string startingAt = Console.ReadLine();
+
+			Console.WriteLine("Where do you want to go to?");
+			string goingTo = Console.ReadLine();
+
+			BusRoute[] originRoutes = repository.FindBusesTo(startingAt);
+			BusRoute[] destinationRoutes = repository.FindBusesTo(goingTo);
+
+			HashSet<BusRoute> routes = new HashSet<BusRoute>(originRoutes);
+			routes.IntersectWith(destinationRoutes);
+
+			if (routes.Count > 0)
+				foreach (BusRoute route in routes)
+					Console.WriteLine($"You can use route {route}");
+			else
+				Console.WriteLine($"No routes go between {startingAt} and {goingTo}");
+		}
+	}
+}
