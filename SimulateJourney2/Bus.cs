e471881fedf93bf309aaed7282f5e762d06a1af9
josemmartinluque <jose.m.martin.luque@avanade.com>
2024-04-05 08:55:32 +0200

    Module 9 completed

---
+++ SimulateJourney2/Bus.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluralsight.ArraysCollections.Demos
+{
+    public class Bus
+    {
+        public const int Capacity = 5;
+        public int Space { get => Capacity - _passengers.Count; }
+
+        private LinkedList<Passenger> _passengers = new LinkedList<Passenger>();
+        public bool Load(Passenger passenger)
+        {
+            if (Space < 1)
+                return false;
+
+            _passengers.AddLast(passenger);
+            Console.WriteLine($"{passenger} got on the bus");
+            return true;
+        }
+        public void ArriveAt(string place)
+        {
+            Console.WriteLine($"\r\nBus arriving at {place}");
+            if (_passengers.Count == 0)
+                return;
+
+            LinkedListNode<Passenger> currentNode = _passengers.First;
+            do{
+                LinkedListNode<Passenger> nextNode = currentNode.Next;
+                if (currentNode.Value.Destination == place)
+                {
+                    Console.WriteLine($"{currentNode.Value} is getting off");
+                    _passengers.Remove(currentNode);
+                }
+                currentNode = nextNode;
+            } while (currentNode != null);
+            Console.WriteLine($"{_passengers.Count} people left on the bus");
+        }
+    }
+}
---
+++ SimulateJourney2/BusStop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluralsight.ArraysCollections.Demos
+{
+    public class BusStop
+    {
+        private Queue<Passenger> _peopleWaiting = new Queue<Passenger>();
+        public void PersonArrive(Passenger passenger)
+        {
+            _peopleWaiting.Enqueue(passenger);
+            Console.WriteLine($"{passenger} queuing at bus stop");
+        }
+        public void BusArrive(Bus bus)
+        {
+            Console.WriteLine("\r\nBus arriving at bus stop to load passengers");
+            while (bus.Space > 0 && _peopleWaiting.Count > 0)
+            {
+                Passenger passenger = _peopleWaiting.Dequeue();
+                bus.Load(passenger);
+            }
+        }
+    }
+}
