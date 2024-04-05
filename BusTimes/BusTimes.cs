b0df29bfb94d08dfbe1156b06f94f5120fc577f0
josemmartinluque <jose.m.martin.luque@avanade.com>
2024-04-05 08:26:47 +0200

    Module 7 completed

---
+++ BusTimes/BusTimes.cs
@@ -0,0 +1,13 @@
+﻿namespace Pluralsight.ArraysCollections.Demos
+{
+    public class BusTimes
+	{
+		public string[][] Times { get; }
+		public BusRoute Route { get; }
+		public BusTimes(BusRoute route, string[][] times)
+		{
+			this.Route = route;
+			this.Times = times;
+		}
+	}
+}
